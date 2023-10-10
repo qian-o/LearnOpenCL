085d66d2195a60cee82ec413ed37ca13c6d5800d
王 熙 <1324771795@qq.com>
2023-10-10 15:46:49 +0800

    更新一维空间计算示例

--- OneDNumCalc/Code.cs
+++ OneDNumCalc/Code.cs
@@ -1,6 +1,7 @@
 ﻿using Core;
 using Core.Helpers;
 using Silk.NET.OpenCL;
+using System.Diagnostics;
 
 namespace OneDNumCalc;
 
@@ -36,87 +37,120 @@ class Code
         }
 
         string source = @"
-            kernel void add(global const int* a, global const int* b, global int* c)
+            kernel void calculateGCD(global const int* a, global const int* b, global int* c)
             {
                 int i = get_global_id(0);
-                c[i] = a[i] + b[i];
-            }
 
-            kernel void mul(global const int* a, global const int* b, global int* c)
-            {
-                int i = get_global_id(0);
-                c[i] = a[i] * b[i];
+                int number1 = a[i];
+                int number2 = b[i];
+    
+                while (number2 != 0)
+                {
+                    int remainder = number1 % number2;
+                    number1 = number2;
+                    number2 = remainder;
+                }
+    
+                c[i] = number1;
             }";
 
         Program program = platforms.First().Value.First(item => item.Type == DeviceType.Gpu).CreateProgram(source);
 
-        TestAdd(program);
-        TestMul(program);
+        Stopwatch stopwatch = Stopwatch.StartNew();
+
+        for (int i = 0; i < 5; i++)
+        {
+            stopwatch.Restart();
+
+            CalculateGCD(program, 100000);
+            CalculateGCD(program, 100000);
+            CalculateGCD(program, 100000);
+
+            Console.WriteLine($"GPU Parallel execution time: {stopwatch.ElapsedMilliseconds} milliseconds");
+        }
+
+        Console.WriteLine();
+
+        for (int i = 0; i < 5; i++)
+        {
+            stopwatch.Restart();
+
+            CalculateGCD(100000);
+            CalculateGCD(100000);
+            CalculateGCD(100000);
+
+            Console.WriteLine($"CPU Parallel execution time: {stopwatch.ElapsedMilliseconds} milliseconds");
+        }
+
+        Console.ReadKey();
     }
 
-    private static unsafe void TestAdd(Program program)
+    /// <summary>
+    /// OpenCL implementation of GCD calculation.
+    /// </summary>
+    /// <param name="program"></param>
+    /// <param name="length"></param>
+    private static unsafe void CalculateGCD(Program program, uint length)
     {
-        Kernel kernel = program.GetKernel("add");
+        Kernel kernel = program.GetKernel("calculateGCD");
 
-        nint memA = program.Device.CreateBuffer<int>(1024, MemFlags.ReadOnly);
-        nint memB = program.Device.CreateBuffer<int>(1024, MemFlags.ReadOnly);
-        nint memC = program.Device.CreateBuffer<int>(1024, MemFlags.WriteOnly);
+        nint memA = program.Device.CreateBuffer<int>(length, MemFlags.ReadOnly);
+        nint memB = program.Device.CreateBuffer<int>(length, MemFlags.ReadOnly);
+        nint memC = program.Device.CreateBuffer<int>(length, MemFlags.WriteOnly);
 
         kernel.SetArgument(0, memA);
         kernel.SetArgument(1, memB);
         kernel.SetArgument(2, memC);
 
-        int* a = stackalloc int[1024];
-        int* b = stackalloc int[1024];
-        int* c = stackalloc int[1024];
-        for (int i = 0; i < 1024; i++)
+        int* a = stackalloc int[(int)length];
+        int* b = stackalloc int[(int)length];
+        int* c = stackalloc int[(int)length];
+        for (int i = 0; i < length; i++)
         {
             a[i] = i;
             b[i] = i;
         }
 
-        program.Device.WriteBuffer<int>(memA, 1024, a);
-        program.Device.WriteBuffer<int>(memB, 1024, b);
+        program.Device.WriteBuffer<int>(memA, length, a);
+        program.Device.WriteBuffer<int>(memB, length, b);
 
-        kernel.Run(1, 1024);
+        kernel.Run(1, length);
 
-        program.Device.ReadBuffer<int>(memC, 1024, c);
+        program.Device.ReadBuffer<int>(memC, length, c);
 
         program.Device.DeleteBuffer(memA);
         program.Device.DeleteBuffer(memB);
         program.Device.DeleteBuffer(memC);
     }
 
-    private static unsafe void TestMul(Program program)
+    /// <summary>
+    /// CPU implementation of GCD calculation.
+    /// </summary>
+    /// <param name="length"></param>
+    private static unsafe void CalculateGCD(uint length)
     {
-        Kernel kernel = program.GetKernel("mul");
-
-        nint memA = program.Device.CreateBuffer<int>(1024, MemFlags.ReadOnly);
-        nint memB = program.Device.CreateBuffer<int>(1024, MemFlags.ReadOnly);
-        nint memC = program.Device.CreateBuffer<int>(1024, MemFlags.WriteOnly);
-
-        kernel.SetArgument(0, memA);
-        kernel.SetArgument(1, memB);
-        kernel.SetArgument(2, memC);
-
-        int* a = stackalloc int[1024];
-        int* b = stackalloc int[1024];
-        int* c = stackalloc int[1024];
-        for (int i = 0; i < 1024; i++)
+        int* a = stackalloc int[(int)length];
+        int* b = stackalloc int[(int)length];
+        int* c = stackalloc int[(int)length];
+        for (int i = 0; i < length; i++)
         {
             a[i] = i;
             b[i] = i;
         }
 
-        program.Device.WriteBuffer<int>(memA, 1024, a);
-        program.Device.WriteBuffer<int>(memB, 1024, b);
+        Parallel.For(0, length, (i) =>
+        {
+            int number1 = a[i];
+            int number2 = b[i];
 
-        kernel.Run(1, 1024);
+            while (number2 != 0)
+            {
+                int remainder = number1 % number2;
+                number1 = number2;
+                number2 = remainder;
+            }
 
-        program.Device.ReadBuffer<int>(memC, 1024, c);
-
-        program.Device.DeleteBuffer(memA);
-        program.Device.DeleteBuffer(memB);
-        program.Device.DeleteBuffer(memC);
+            c[i] = number1;
+        });
     }
 }
